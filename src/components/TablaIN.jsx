a293ba8f8a3e89a1d14150eeee3202be97595192
Uchihabois45FWD <fwdadriandcc0607@gmail.com>
2025-09-19 09:07:22 -0600

    Inicio

---
+++ src/components/TablaIN.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function TablaIN() {
+  return (
+    <div>
+        <h1>Tabla de Inicio</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Nombre</th>
+                    <th>Apellido</th>
+                    <th>Edad</th>
+                    <th>Genero</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Nombre</td>
+                    <td>Apellido</td>
+                    <td>Edad</td>
+                    <td>Genero</td>
+                    <td>Acciones</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+  )
+}
+
+export default TablaIN
---
+++ src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TablaIN from '../components/TablaIN'
+
+function Login() {
+  return (
+    <div>
+        <TablaIN/>
+    </div>
+  )
+}
+
+export default Login
